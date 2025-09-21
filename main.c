998e42cfa3400fe7c4789aaac5195719202e14dc
wngydls-lang <wngydls@sookmyung.ac.kr>
2025-09-21 17:41:21 +0900

    3주차 실습2

--- main.c
+++ main.c
@@ -1,21 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
-{
-  int main(void) {
-      int input_int;
-      float input_float;
+int main(int argc, char *argv[]) {
       
-      printf("enter an integer : ");
-      scanf("%d", &input_int);
+      char c;
+      int i;
       
-      printf("enter a float : ");
-      scanf("%f", &input_float);
+      printf("input a number : ");
+      scanf("%c", &c);
       
-      printf("integer : %d, float : %f\n", input_int, input_float);
+      i = c - '0';
+      printf("The input number is %i\n", i);
       
       system("pause");
       return 0;
+      
 }
-}
+
